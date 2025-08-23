60a6dd6362e2b1dc45af0facf402eef55652a678
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-08-23 12:04:43 +0630

    Add project files.

---
+++ SLHDotNetTrainingInPersonBatch1_1.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+string a = "";
+
+// Ctrl + .
+
+//Ctrl + K, C
+//Ctrl + K, U
+
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+sqlConnectionStringBuilder.DataSource = "."; // Server name
+sqlConnectionStringBuilder.InitialCatalog = "SLHDotNetTrainingInPersonBatch1"; // Database name
+sqlConnectionStringBuilder.UserID = "sa"; // User name
+sqlConnectionStringBuilder.Password = "sasa@123"; // Password
+sqlConnectionStringBuilder.TrustServerCertificate = true; // For local development
+
+Console.WriteLine(sqlConnectionStringBuilder.ConnectionString);
+
+SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+connection.Open();
+connection.Close();
+
+Console.ReadLine();
+//Console.ReadKey();
